19e2fa8b790e57506f3d8b7ed65232cb4ec682e0
Ahoussi Armand SAY <asay@pc-df-892.priv.enst-bretagne.fr>
2017-01-20 15:16:25 +0100

    starting GeneralizedBlackScholesProcess initialization

--- project4/main.cpp
+++ project4/main.cpp
@@ -12,6 +12,30 @@ int main() {
 
         // add your code here
 
+        // GeneralizedBlackScholesProcess process argument (5) 
+        		
+				// const Handle<Quote>& x0  , 
+       			// Handle<YieldTermStructure>& dividendTS , 
+                // Handle<YieldTermStructure>& riskFreeTS,
+                // Handle<BlackVolTermStructure>& blackVolTS,
+                //const boost::shared_ptr<discretization>& d =boost::shared_ptr<discretization>()
+
+        // instaciate a GeneralizedBlackscholes process with the 5 arguments   
+           GeneralizedBlackScholesProcess * prr = new GeneralizedBlackScholesProcess () ; 
+		// add the stocastique process 
+    boost::shared_ptr<GeneralizedBlackScholesProcess>  process (prr); 
+        // initialised a wanted timestep 
+    Size timeSteps (30) ;
+        // initialised the Binomial engine 
+    BinomialVanillaEngine_2 B2eng  = new  BinomialVanillaEngine_2 ( process , timeSteps) ; 
+        // call the calculate method   
+    B2eng->calculate() ; 
+        // evaluate the result 
+
+      
+
+
+
         return 0;
 
     } catch (std::exception& e) {
